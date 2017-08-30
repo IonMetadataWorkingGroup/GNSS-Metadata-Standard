ea8b275005035ee21795114f2750c66b0a1fbfb9
jamestcurran <jamestcurran@gmail.com>
2017-08-30 19:16:26 +0200

    Updated SampleInterpreterFactory

--- source/converter/inc/SampleInterpreterFactory.hpp
+++ source/converter/inc/SampleInterpreterFactory.hpp
@@ -92,13 +92,13 @@ bool SampleInterpreterFactory<chunk_t,sample_base_t>::Create(
    //it would be possible here to create specializations, which combine FormatFunctions and EncoderFunctions
    
    //try Int8 IF
-   if( (fmt == GnssMetadata::IonStream::IQ) && ( enc == "TC" ) && ( BitWidth( fmt, qnt ) == 8 ) )
+   if( (fmt == GnssMetadata::IonStream::IF) && ( enc == "TC" ) && ( BitWidth( fmt, qnt ) == 8 ) )
    {
       //otherwise proceed with a generic SampleInterpreter
       smplIntrp = new SinkedSampleInterpreter<chunk_t,sample_base_t>(
                                                                      BitWidth( fmt, qnt ),
                                                                      mEncoderFunctionMap["INT8"],
-                                                                     mFormatFunctionMap[ GnssMetadata::IonStream::IQ ],
+                                                                     mFormatFunctionMap[ GnssMetadata::IonStream::IF ],
                                                                      sampleSink,
                                                                      callOrder
                                                                      );
